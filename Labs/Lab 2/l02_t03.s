20323566ce5f522b86f036e99083708821ce995b
Ryan Chisholm <71892582+Chisholm6192@users.noreply.github.com>
2024-02-08 23:04:02 -0500

    Add files via upload

---
+++ Labs/Lab 2/l02_t03.s
@@ -0,0 +1,45 @@
+/*
+-------------------------------------------------------
+l02_t03.s
+-------------------------------------------------------
+Author: Ryan Chisholm
+ID: 169027577
+Email: chis7577@mylaurier.ca
+Date: 01-30-2024
+-------------------------------------------------------
+Copies contents of one vector to another.
+-------------------------------------------------------
+*/
+.org 0x1000  // Start at memory location 1000
+.text        // Code section
+.global _start
+_start:
+
+.text // code section
+// Copy contents of first element of Vec1 to Vec2
+ldr r0, =Vec1;
+ldr r1, =Vec2;
+ldr r2, [r0]
+str r2, [r1]
+// Copy contents of second element of Vec1 to Vec2
+add r0, r0, #4
+add r1, r1, #4
+ldr r2, [r0]
+str r2, [r1]
+// Copy contents of second element of Vec1 to Vec2
+add r1, r1, #4
+add r0, r0, #4
+ldr r2, [r0]
+str r2, [r1]
+// End program
+_stop:
+b _stop
+
+.data // Initialized data section
+Vec1:
+.word 1, 2, 3
+.bss // Uninitialized data section
+Vec2:
+.space 12
+
+.end
